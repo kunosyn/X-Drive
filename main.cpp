690566afdee9338f054604278d184f27c993e0aa
kunosyn <65258314+kunosyn@users.noreply.github.com>
2021-10-15 07:19:19 -0500

    Initial commit.
    Added all code.

---
+++ main.cpp
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       {author}                                                  */
+/*    Created:      {date}                                                    */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// Include the V5 Library
+#include "vex.h"
+  
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+/*
+    Left back motor: 4
+    Left front motor: 1
+
+    Right back motor: 6
+    Right front motor: 10
+*/
+void init_motors()
+{
+    RBMotor.setVelocity(0, percent);
+    RFMotor.setVelocity(0, percent);
+    LBMotor.setVelocity(0, percent);
+    LFMotor.setVelocity(0, percent);
+
+    RBMotor.spin(forward);
+    RFMotor.spin(forward);
+    LBMotor.spin(forward);
+    LFMotor.spin(forward);
+}
+
+int main() {
+
+    init_motors(); 
+
+    do {
+        int axis3Pos = Controller1.Axis3.position(), axis1Pos = Controller1.Axis1.position(), axis4Pos = Controller1.Axis4.position();
+
+        LFMotor.setVelocity(axis3Pos + axis1Pos + axis4Pos, percent);
+        LBMotor.setVelocity(-axis3Pos - axis1Pos + axis4Pos, percent);
+
+        RFMotor.setVelocity(axis3Pos + axis1Pos + axis4Pos, percent);
+        RBMotor.setVelocity(-axis3Pos - axis1Pos + axis4Pos, percent);
+    } while(true);
+}
